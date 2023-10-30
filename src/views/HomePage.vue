4b40f94d506cdad76485f9e119e16357ed10b1a2
Jose Angel <82170126+jose-angel-developer@users.noreply.github.com>
2023-10-30 13:34:53 -0400

    Primer Commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+<ion-page>
+    <ion-header>
+        <ion-toolbar>
+            <ion-title>
+                Cars
+            </ion-title>
+        </ion-toolbar>
+    </ion-header>
+</ion-page>
+</template>
+<script>
+
+import {IonPage, IonGrid, IonRow, IonCol, IonToolbar, IonTitle,} from '@ionic/vue';
+
+export default {
+  name: 'HomePage',
+  components:{
+    IonCol,
+    IonPage,
+    IonGrid,
+    IonRow,
+    IonToolbar,
+    IonTitle
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomePage2.vue
@@ -0,0 +1,89 @@
+<template>
+  <!--Pagina de ionic-->
+  <ion-page>
+    <h1>Grid en Ionic</h1>
+    <ion-grid>
+      <ion-row>
+        <ion-col size="4" offset="1">
+          <div class="image-container">
+            <img src="/rayo.png" alt="" srcset="">
+            <p>Lorem ipsum dolor sit, amet consectetur 
+              adipisicing elit. Tempora harum vitae, nobis 
+              iure porro cupiditate voluptas impedit 
+              esse molestias? Dolores nulla praesentium 
+              quisquam, incidunt eius laudantium quam 
+              commodi labore repudiandae!</p>
+          </div>
+        </ion-col>
+        <ion-col size="6">
+          <div class="image-container">
+            <img src="/mater.png" alt="" srcset="">
+            <p>Lorem ipsum dolor sit, amet consectetur 
+              adipisicing elit. Tempora harum vitae, nobis 
+              iure porro cupiditate voluptas impedit 
+              esse molestias? Dolores nulla praesentium 
+              quisquam, incidunt eius laudantium quam 
+              commodi labore repudiandae!
+              Lorem ipsum dolor sit, amet consectetur 
+              adipisicing elit. Tempora harum vitae, nobis 
+              iure porro cupiditate voluptas impedit 
+              esse molestias? Dolores nulla praesentium 
+              quisquam, incidunt eius laudantium quam 
+              commodi labore repudiandae!
+              Lorem ipsum dolor sit, amet consectetur 
+              adipisicing elit. Tempora harum vitae, nobis 
+              iure porro cupiditate voluptas impedit 
+              esse molestias? Dolores nulla praesentium 
+              quisquam, incidunt eius laudantium quam 
+              commodi labore repudiandae!
+            </p>
+          </div>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-page>
+</template>
+
+<script>
+
+  import {IonPage, IonGrid, IonRow, IonCol} from '@ionic/vue';
+
+export default {
+  name: 'HomePage',
+  components:{
+    IonCol,
+    IonPage,
+    IonGrid,
+    IonRow
+
+  }
+
+}
+</script>
+
+<style>
+ion-grid{
+  margin: auto;
+  border: 1px solid black;
+}
+.image-container{
+  max-width: 100%;
+  max-height: 100%;
+  display: block;
+  margin: auto;
+  border: 1px solid black;
+}
+
+img{
+  max-width: 80%;
+  height: 90px;
+}
+ion-row{
+  text-align: center;
+  height: 100px;
+}
+
+h1{
+  text-align: center;
+}
+</style>
